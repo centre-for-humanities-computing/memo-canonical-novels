356b5f52fa225702df9ef1dd6d20e989ceb82dcd
jankounchained <honza.anfas@gmail.com>
2024-08-09 14:48:16 +0200

    tmp: louises code backup

--- test_info.py
+++ test_info.py
@@ -9,6 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datasets import Dataset
 from sklearn.decomposition import PCA
+from tslearn.metrics import dtw
 from infodynamics import WindowedRollingDistance
 from infodynamics.util import calc_vector_histogram
 
@@ -71,3 +72,54 @@ import seaborn as sns
 sns.heatmap(D, cmap="mako")
 
 # %%
+# louise's dtw script
+def calc_signal_difference(no_artist_signal_df, org_signal, w_size):
+
+    # convert the novelty signal excluding a specific artist to a list
+    n_df = no_artist_signal_df.iloc[w_size:, :]
+    n_hat = n_df['N_hat'].tolist()
+
+    # do the same for the original novelty signal
+    n_df_original = org_signal.iloc[w_size:, :]
+    n_hat_original = n_df_original['N_hat'].tolist()
+
+    # calculate dtw (Dynamic Time Warping) difference
+    dtw_score_n = dtw(n_hat, n_hat_original)
+
+    # convert the resonance signal excluding a specific artist to a list
+    r_df = no_artist_signal_df.iloc[w_size:-w_size, :]
+    r_hat = r_df['R_hat'].tolist()
+
+    # do the same for the original resonance signal
+    r_df_original = org_signal.iloc[w_size:-w_size, :]
+    r_hat_original = r_df_original['R_hat'].tolist()
+
+    # calculate difference
+    dtw_score_r = dtw(r_hat, r_hat_original)
+
+    return dtw_score_n, dtw_score_r
+
+
+def calc_dtw_scores(ds, artist, w_size, org_signal):
+
+    # remove artist from data
+    ds_no_artist = remove_artist(ds, artist)
+
+    # calculate new signal with artist removed
+    signal_df = calc_signals(ds_no_artist, w_size)
+
+    # calculate dtw scores for novelty and resonance for the new and original signal
+    dtw_score_n, dtw_score_r = calc_signal_difference(signal_df, org_signal, w_size)
+
+    return dtw_score_n, dtw_score_r
+
+
+# %%
+dtw_n = []
+dtw_r = []
+
+for artist in grouped_df['artist']:
+    dtw_scores = calc_dtw_scores(ds, artist, 20, org_signal) # using window size of 20
+
+    dtw_n.append(dtw_scores[0])
+    dtw_r.append(dtw_scores[1])
