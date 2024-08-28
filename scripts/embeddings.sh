f2ebf264ace91aa83138187733791f535fa05dcf
jankounchained <honza.anfas@gmail.com>
2024-08-28 18:00:34 +0200

    fix: add old models to call

--- scripts/embeddings.sh
+++ scripts/embeddings.sh
@@ -1,3 +1,17 @@
+python src/embeddings.py \
+    --model-name "MiMe-MeMo/MeMo-BERT-03" \
+    --max-tokens 512
+
+python src/embeddings.py \
+    --model-name "KennethEnevoldsen/dfm-sentence-encoder-large-exp2-no-lang-align" \
+    --max-tokens 512
+
+python src/embeddings.py \
+    --model-name "intfloat/multilingual-e5-large" \
+    --max-tokens 510 \
+    --prefix "Query: " \
+    --prefix-description ""
+
 # prompt is 87 tokens, max-tokens = 512-87
 python src/embeddings.py \
     --model-name "intfloat/multilingual-e5-large-instruct" \
@@ -5,9 +19,9 @@ python src/embeddings.py \
     --prefix "Instruct: Identify the author of a given passage from historical Danish fiction\nQuery: " \
     --prefix-description "identify_author"
 
-# prompt is 89 tokens, max-tokens = 512-89
+# prompt is 91 tokens, max-tokens = 512-89
 python src/embeddings.py \
     --model-name "intfloat/multilingual-e5-large-instruct" \
-    --max-tokens 423 \
-    --prefix "Instruct: Given a passage from historical Danish fiction, retrieve similar texts \nQuery: " \
+    --max-tokens 421 \
+    --prefix "Instruct: Retrieve semantically similar texts given a passage in historical Danish \nQuery: " \
     --prefix-description "retrieve"
